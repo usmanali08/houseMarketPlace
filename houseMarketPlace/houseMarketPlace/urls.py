9f82fc9cdf68346a761d54783c24c7c9d0ba6361
usmanali08 <usmanali08@protonmail.com>
2018-03-18 17:36:08 +0530

    register app added

--- houseMarketPlace/houseMarketPlace/urls.py
+++ houseMarketPlace/houseMarketPlace/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from login.views import login
+from register.views import register
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', login),
-    url(r'^login/', login)
+    url(r'^login/', login),
+    url(r'^register/', register),
 ]
---
+++ houseMarketPlace/register/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def register(request):
+  return render(request, 'register/register.html')
