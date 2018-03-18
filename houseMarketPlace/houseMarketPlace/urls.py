d96a4277a09f0842a4cc3a5243ab6ad3f8c50909
usmanali08 <usmanali08@protonmail.com>
2018-03-18 16:47:07 +0530

    login form added

--- houseMarketPlace/houseMarketPlace/urls.py
+++ houseMarketPlace/houseMarketPlace/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from login.views import login
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', login),
+    url(r'^login/', login)
 ]
